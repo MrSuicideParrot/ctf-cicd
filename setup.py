c2e15dddf10b4a93f2421bd0e8bb83db808cdb7f
André Cirne <MrSuicideParrot@users.noreply.github.com>
2021-09-21 16:07:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIRES = [
 
 setup(
     name='ctfcicd',
-    version='0.1.5',
+    version='0.1.6',
     install_requires=REQUIRES,
     packages=find_packages(),
     url='https://github.com/MrSuicideParrot/ctf-cicd',
