d209502b32e500aa400c6075f45dfb4365451a56
André Cirne <dumahk21@gmail.com>
2023-09-06 15:27:43 +0100

    Remove unecessary imports and way to specify dir

--- ctfcicd/__main__.py
+++ ctfcicd/__main__.py
@@ -1,15 +1,18 @@
-from ast import parse
 from ctfcicd import CiCd
-from sys import argv
-from os import getenv
 from argparse import ArgumentParser, BooleanOptionalAction
+from os import chdir
 
 def main():
     parser = ArgumentParser(description='Deploy challenges in ctfd')
     parser.add_argument('--insecure-tls-verification', action=BooleanOptionalAction, default=False)
     parser.add_argument('--testing', action=BooleanOptionalAction, default=False)
     parser.add_argument('--commits', action='extend', nargs='*', type=str)
+    parser.add_argument('--directory', help="CTF directory")
     args = parser.parse_args()
+
+    if args.directory is not None:
+        chdir(args.directory)
+
 
     if args.commits is not None and len(args.commits) == 2:
         CiCd(not args.testing, not args.insecure_tls_verification).sync_folder_with_git(*args.commits)
